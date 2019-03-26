1cb187e534d2e153e53bd60adc2ca653c3e08f99
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-03-26 14:18:11 +0200

    add where to apload the image

--- photos/urls.py
+++ photos/urls.py
@@ -7,7 +7,8 @@ urlpatterns=[
     url(r'^$', views.photos_today, name = 'photosToday'),
     # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastNews'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^new/image$', views.new_image, name='new-image')
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^upload/profile', views.upload_profile, name='upload_profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
